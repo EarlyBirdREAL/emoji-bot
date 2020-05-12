0f931eee0afdc1b06c00e366089356af59725848
EarlyBirdREAL <44191940+EarlyBirdREAL@users.noreply.github.com>
2020-05-12 13:13:28 +0200

    Add files via upload

--- emojibot.js
+++ emojibot.js
@@ -1,5 +1,6 @@
-const { Client, MessageEmbed } = require('discord.js');
-const client = new Client();
+const Discord = require('discord.js');
+const { Cli, MessageEmbed } = require('discord.js');
+const client = new Discord.Client();
 
 client.once('ready', () => {
     console.log('Ready!');
