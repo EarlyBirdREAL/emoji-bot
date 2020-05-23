400ce40fe7422da88f28fc874eb7463dbf245240
EarlyBirdREAL <44191940+EarlyBirdREAL@users.noreply.github.com>
2020-05-23 13:20:36 +0200

    Add files via upload

--- emojibot.js
+++ emojibot.js
@@ -362,11 +362,11 @@ server.on('message', async message => {
         })
     }
     if (message.content.startsWith("!config init")) {
-        client.query(`SELECT user_name FROM d6emhm7uh31vi7 WHERE user_name='${message.author.id}';`, (err, res) => {
-            if (err) client.query(`INSERT INTO d6emhm7uh31vi7 (user_name, enable) VALUES (${message.author.id}, true)`);
+        client.query(`SELECT user_name FROM enlarge WHERE user_name='${message.author.id}';`, (err, res) => {
+            if (err) client.query(`INSERT INTO enlarge (user_name, enable) VALUES (${message.author.id}, true)`);
             console.log(err)
             if (!err) {
-                client.query(`UPDATE d6emhm7uh31vi7 SET enable = true WHERE user_name = '${message.author.id}`)
+                client.query(`UPDATE enlarge SET enable = true WHERE user_name = '${message.author.id}`)
             }
             client.end();
         });
