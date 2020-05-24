2968b2ebccb54e6d9afff22c72944ef6a21782db
EarlyBirdREAL <44191940+EarlyBirdREAL@users.noreply.github.com>
2020-05-24 23:00:40 +0200

    Add files via upload

--- emojibot.js
+++ emojibot.js
@@ -25,6 +25,7 @@ server.on('message', async message => {
             console.log(res.rows["enable"])
             for (let row of res.rows) {
                 console.log(row)
+                console.log(row.enable)
                 if (row.enable == "true") {
                     if (message.content.includes(":b99:")) {
                         message.channel.send('', {
